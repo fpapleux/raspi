dd41d6bfac0ef8f699305979ed217fb748c6b9de
Fabien Papleux <fabien@papleux.com>
2015-02-07 23:03:48 -0500

    add

--- init.sh
+++ init.sh
@@ -26,15 +26,16 @@ fi
 ## System Settings
 #####################################################################################
 
-echo -n "Set US Keyboard ('y' for yes) ? "
+echo -n "Set US locale and keyboard ('y' for yes) ? "
 read -n 1 q
 echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
 	echo "."
 	echo "."
 	echo "."
-	echo "Setting US Keyboard"
+	echo "Setting US locale and keyboard"
 	echo "---------------------------------------------------------------------------------"
+	sudo cp -f files/locale.gen /etc/locale.gen
 	sudo cp -f files/keyboard /etc/default/keyboard
 fi
 
