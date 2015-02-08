47471dd8563c378b5347bcd45fe37a5721f4e08d
Fabien Papleux <fabien@papleux.com>
2015-02-07 21:49:47 -0500

    add

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Raspi Repo
+#
+# Raspberry Pi Repository
+#
+
+Feel free to download the content of this repo to your raspberry pi and run init.sh to initialize it with the default settings that I use.
+
+1. Make sure your raspberry pi is connected to the internet.
+
+2. Type:
+<code>
+	mkdir raspi
+	cd raspi
+	git init
+	git clone https://github.com/fpapleux/raspi
+</code>
+
---
+++ init.sh
@@ -0,0 +1,20 @@
+echo "Initializing new raspberry pi... -- Make sure the raspberry pi is connected to the Internet..."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "Updating APT-GET libraries and installed packages"
+echo "---------------------------------------------------------------------------------"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+echo "."
+echo "."
+echo "."
+echo "Setting US Keyboard"
+echo "---------------------------------------------------------------------------------"
+sudo setxkbmap us
+
+
