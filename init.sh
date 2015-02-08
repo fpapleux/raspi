f50b1dd3d772284c908a5ebfb58bdb156cca38bd
Fabien Papleux <fabien@papleux.com>
2015-02-07 22:44:42 -0500

    add

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ echo "."
 
 echo -n "Refresh apt-get from library ('y' for yes) ? "
 read -n 1 q
-if [ $q eq 'y' ] || [ $q eq 'Y' ]; then
+if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
 	echo "."
 	echo "."
 	echo "."
