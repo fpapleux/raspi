fef78034a4647f64bf85689ceb5e872ed6940207
Fabien Papleux <fabien@papleux.com>
2015-02-17 10:00:43 -0500

    Delete update-system.sh

--- scripts/update-system.sh
+++
@@ -1,8 +0,0 @@
-echo "."
-echo "."
-echo "."
-echo "Updating APT-GET libraries and installed packages"
-echo "---------------------------------------------------------------------------------"
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
