530d787e6217e0540c44ebe491a2a3a018d234c1
Fabien Papleux <fabien@papleux.com>
2015-02-18 21:40:58 -0500

    more stuff

--- raspi.sh
+++ raspi.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "Initializing new raspberry pi... -- Make sure the raspberry pi is connected to the Internet..."
-echo "."
-echo "."
-echo "."
+echo "\n\n\n"
 reboot=0
 
 
@@ -15,18 +13,11 @@ reboot=0
 echo -n "Refresh system and apt-get from library ('y' for yes) ? "
 read -n 1 q; echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
-	echo "."
-	echo "."
-	echo "."
-	echo "Updating APT-GET libraries and installed packages"
-	echo "---------------------------------------------------------------------------------"
+	echo -n "\n\nUpdating APT-GET libraries and installed packages... "
 	sudo apt-get -y update 								# Update library
 	sudo apt-get -y upgrade 							# Upgrade all local libraries
-	sudo apt-get -y install build-essential				# Essentials compilers, etc.
-	sudo apt-get -y install wireless-tools				# drivers for wifi
-	sudo apt-get -y install wpasupplicant				# Other command for wifi drivers
+	echo "done\n\n"
 fi
-
 
 
 
@@ -40,6 +31,10 @@ fi
 echo -n "Set up wireless adapter ('y' for yes) ? "
 read -n 1 q; echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
+
+	sudo apt-get -y install wireless-tools				# drivers for wifi
+	sudo apt-get -y install wpasupplicant				# Other command for wifi drivers
+
 	interface="$(ifconfig -a | grep -i wlan | cut -c1-5)"
 	if [ "$interface" == "" ]; then
 		echo "---------------------------------------------------------------------------------"
@@ -48,18 +43,13 @@ if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
 		echo "------------------------ PRESS ANY KEY TO CONTINUE ------------------------------"
 		read -n 1 q; echo
 	else
-		echo -n "Enter your SSID: "
-		read ssid; echo
-		echo -n "Enter your WPA key: "
-		read wpa; echo
-		echo "."
-		echo "."
-		echo "."
-		echo "Setting up wireless networking"
-		echo "---------------------------------------------------------------------------------"
+		echo -n "Enter your SSID: "; read ssid
+		echo -n "Enter your WPA key: "; read wpa
+		echo -n "\n\nSetting up wireless networking... "
 		cat files/interfaces | sed -e "s/\#INTERFACE/$interface/" -e "s/\#SSID/$ssid/" -e "s/\#WPA/$wpa/" > ./interfaces
 		sudo mv -f ./interfaces /etc/network/
 		reboot=1
+		echo "done\n\n"
 	fi
 fi
 
@@ -68,21 +58,17 @@ fi
 
 
 
-
 #####################################################################################
-## System Settings
+## ADJUSTING SYSTEM SETTINGS
 #####################################################################################
 
 echo -n "Set US locale and keyboard ('y' for yes) ? "
 read -n 1 q; echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
-	echo "."
-	echo "."
-	echo "."
-	echo "Setting US locale and keyboard"
-	echo "---------------------------------------------------------------------------------"
+	echo -n "\n\nSetting US locale and keyboard... "
 	sudo cp -f files/locale.gen /etc/locale.gen 		# Set locale to US
 	sudo cp -f files/keyboard /etc/default/keyboard		# Set keyboard layout to US
+	echo "done\n\n"
 fi
 
 
@@ -91,26 +77,31 @@ fi
 
 
 #####################################################################################
-## Setting User Environment
+## SETTING UP USER ENVIRONMENT
 #####################################################################################
 
-## INCOMPLETE
+# Manage users
 
-echo -n "Setting user environment ('y' for yes) ? "
-read -n 1 q; echo
-if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
-	echo "."
-	echo "."
-	echo "."
-	echo "Setting up user environment"
-	echo "---------------------------------------------------------------------------------"
-	echo -n "Enter user name [end with ENTER]: "
-	read user; echo
-	echo -n "Enter password for new user [end with ENTER]: "
-	read pwd; echo
+q="y"
+while [ "$q" == "y" ] || [ "$q" == "Y" ]; do
+	echo -n "Need to add a new admin user ('y' for yes) ? "
+	read -n 1 q; echo
+	if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
+		echo -n "Enter username: "; read user
+		echo -n "Enter full name: "; read userFullName
+		echo -n "Enter password: "; read password
+		if [ "$user" != "" ]; then
+			echo -n "\n\nAdding new user... "
+			cat files/userinfo | sed -e "s/\#PASSWORD/$password/" -e "s/\#USERFULLNAME/$userFullName/" -e "s/\#CR/\n/" > ./userinfo
+			sudo adduser --home /home/"$user" "$user" < ./userinfo	# Creating user
+			sudo cp -f files/.bashrc /home/"$user"/					# Set bash environment
+			sudo cp -f files/.nanorc /home/"$user"/					# Set bash environment
+			# Use deluser USER to remove users (deluser --group GROUP for groups)
+			echo "done\n\n"
+		fi
+	fi	
+done
 
-	sudo cp -f files/.bashrc ~							# Set bash environment
-fi
 
 
 
@@ -119,6 +110,29 @@ fi
 #####################################################################################
 ## Setting Tools & Development Environment
 #####################################################################################
+
+# 1. Install git
+
+echo -n "Install git ('y' for yes) ? "
+read -n 1 q; echo
+if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
+	echo -n "\n\ninstalling git... "
+	sudo apt-get -y install git-core
+	echo -n "Enter system username to setup: "; read user
+	echo -n "Enter git user.name: "; read gitUser
+	echo -n "Enter git user.email: "; read gitEmail
+	sudo echo "[core]\n    editor = nano\n" > /home/"$user"/.gitconfig
+	sudo echo "[user]\n    name = $gitUser\n" >> /home/"$user"/.gitconfig
+	sudo echo "    email = $gitEmail\n" >> /home/"$user"/.gitconfig
+	echo "done\n\n"
+fi
+
+
+
+	# sudo apt-get -y install build-essential			# Essentials compilers, etc. 
+														# --> ONLY USE with large SD Card
+
+## INCOMPLETE
 
 echo -n "Install Node.js ('y' for yes) ? "
 read -n 1 q; echo
