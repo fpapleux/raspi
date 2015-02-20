d92d2f6f290be34ebc464cb6a104a39337eb68fa
Fabien Papleux <fabien@papleux.com>
2015-02-20 13:30:26 -0500

    more stuff

--- raspi.sh
+++ raspi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Initializing new raspberry pi... -- Make sure the raspberry pi is connected to the Internet..."
-echo "\n\n\n"
+echo -e "\n\n\n"
 reboot=0
 
 
@@ -13,10 +13,10 @@ reboot=0
 echo -n "Refresh system and apt-get from library ('y' for yes) ? "
 read -n 1 q; echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
-	echo -n "\n\nUpdating APT-GET libraries and installed packages... "
+	echo -en "\n\nUpdating APT-GET libraries and installed packages... "
 	sudo apt-get -y update 								# Update library
 	sudo apt-get -y upgrade 							# Upgrade all local libraries
-	echo "done\n\n"
+	echo -e "done\n\n"
 fi
 
 
@@ -45,11 +45,11 @@ if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
 	else
 		echo -n "Enter your SSID: "; read ssid
 		echo -n "Enter your WPA key: "; read wpa
-		echo -n "\n\nSetting up wireless networking... "
+		echo -en "\n\nSetting up wireless networking... "
 		cat files/interfaces | sed -e "s/\#INTERFACE/$interface/" -e "s/\#SSID/$ssid/" -e "s/\#WPA/$wpa/" > ./interfaces
 		sudo mv -f ./interfaces /etc/network/
 		reboot=1
-		echo "done\n\n"
+		echo -e "done\n\n"
 	fi
 fi
 
@@ -65,10 +65,10 @@ fi
 echo -n "Set US locale and keyboard ('y' for yes) ? "
 read -n 1 q; echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
-	echo -n "\n\nSetting US locale and keyboard... "
+	echo -en "\n\nSetting US locale and keyboard... "
 	sudo cp -f files/locale.gen /etc/locale.gen 		# Set locale to US
 	sudo cp -f files/keyboard /etc/default/keyboard		# Set keyboard layout to US
-	echo "done\n\n"
+	echo -e "done\n\n"
 fi
 
 
@@ -91,13 +91,13 @@ while [ "$q" == "y" ] || [ "$q" == "Y" ]; do
 		echo -n "Enter full name: "; read userFullName
 		echo -n "Enter password: "; read password
 		if [ "$user" != "" ]; then
-			echo -n "\n\nAdding new user... "
+			echo -en "\n\nAdding new user... "
 			cat files/userinfo | sed -e "s/\#PASSWORD/$password/" -e "s/\#USERFULLNAME/$userFullName/" -e "s/\#CR/\n/" > ./userinfo
 			sudo adduser --home /home/"$user" "$user" < ./userinfo	# Creating user
 			sudo cp -f files/.bashrc /home/"$user"/					# Set bash environment
 			sudo cp -f files/.nanorc /home/"$user"/					# Set bash environment
 			# Use deluser USER to remove users (deluser --group GROUP for groups)
-			echo "done\n\n"
+			echo -e "done\n\n"
 		fi
 	fi	
 done
@@ -116,15 +116,15 @@ done
 echo -n "Install git ('y' for yes) ? "
 read -n 1 q; echo
 if [ "$q" == "y" ] || [ "$q" == "Y" ]; then
-	echo -n "\n\ninstalling git... "
+	echo -en "\n\ninstalling git... "
 	sudo apt-get -y install git-core
 	echo -n "Enter system username to setup: "; read user
 	echo -n "Enter git user.name: "; read gitUser
 	echo -n "Enter git user.email: "; read gitEmail
-	sudo echo "[core]\n    editor = nano\n" > /home/"$user"/.gitconfig
-	sudo echo "[user]\n    name = $gitUser\n" >> /home/"$user"/.gitconfig
-	sudo echo "    email = $gitEmail\n" >> /home/"$user"/.gitconfig
-	echo "done\n\n"
+	sudo echo -e "[core]\n    editor = nano\n" > /home/"$user"/.gitconfig
+	sudo echo -e "[user]\n    name = $gitUser\n" >> /home/"$user"/.gitconfig
+	sudo echo -e "    email = $gitEmail\n" >> /home/"$user"/.gitconfig
+	echo -e "done\n\n"
 fi
 
 
