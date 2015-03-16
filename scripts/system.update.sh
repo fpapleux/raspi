e8f83468d3bcabe6368793027de8cf910352a235
Fabien Papleux <fabien@papleux.com>
2015-03-16 10:15:20 -0400

    more stuff

--- scripts/system.update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -en "\n\nUpdating APT-GET libraries and installed packages... "
-
-sudo apt-get -y update 								# Update library
-sudo apt-get -y upgrade 							# Upgrade all local libraries
-
-echo -e "\n\ndone\n\n"
