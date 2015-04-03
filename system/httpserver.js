55aa6b2adc0934251256a162e6847786b65e18ef
Fabien Papleux <fabien@papleux.com>
2015-04-03 16:01:31 -0400

    test

---
+++ system/httpserver.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+http.createServer(
+
+	function(request, response) 
+	{
+  		console.log("Request was \n" + )
+  		response.writeHead(200, {"Content-Type": "text/plain"});
+  		response.write("Hello World");
+  		response.end();
+	}
+
+).listen(8888);
